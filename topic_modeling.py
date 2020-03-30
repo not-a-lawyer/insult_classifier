26710a3537bab2f9bea15d7abcb17f9df4f8cf87
Maximilian Kroker <kroker@not-a.lawyer>
2020-03-30 21:45:58 +0200

    created preprocess_data() and stemming_text_samples()

--- topic_modeling.py
+++ topic_modeling.py
@@ -5,4 +5,22 @@ from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.stem.porter import *
 import numpy as np
 
+#What does this do?
+import nltk
+nltk.download('wordnet')
+
 def preprocess_data(text_samples):
+    pass
+
+def stemming_text_samples(text_samples):
+    stemmer = SnowballStemmer("english")
+
+
+    processed_text = []
+
+    for token in gensim.utils.simple_preprocess(text_samples):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            processed_text.append(stemmer.stem(WordNetLemmatizer().lemmatize(text_samples, pos='v')))
+
+    return processed_text
+
