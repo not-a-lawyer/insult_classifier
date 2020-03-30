2692d6997e4902123cde1b0942a4580705a64ff7
Maximilian Kroker <kroker@not-a.lawyer>
2020-03-30 21:31:54 +0200

    added imports

---
+++ topic_modeling.py
@@ -0,0 +1,6 @@
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+import numpy as np
