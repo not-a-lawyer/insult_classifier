c641b37ec5148f693a26764826240edf54b460e2
Maximilian Kroker <kroker@not-a.lawyer>
2020-04-06 17:28:49 +0200

    imported insults.csv

--- rnn.py
+++ rnn.py
@@ -2,3 +2,7 @@
 ## Following this guide at
 # https://medium.com/@armandj.olivares/detecting-toxic-comments-with-keras-and-interpreting-the-model-with-eli5-dbe734f3e86b
 
+from bayes_classifier import *
+
+#import insults.csv. see import_data() for details on data or README
+type_tweet = import_data()
