c155f77e5741b435d39a97cc9723aa39c30edc5e
Maximilian Kroker <kroker@not-a.lawyer>
2020-04-06 17:27:05 +0200

    removed old imports

--- rnn.py
+++ rnn.py
@@ -2,19 +2,3 @@
 ## Following this guide at
 # https://medium.com/@armandj.olivares/detecting-toxic-comments-with-keras-and-interpreting-the-model-with-eli5-dbe734f3e86b
 
-# Import required libraries
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import sklearn
-
-# Import necessary modules
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from math import sqrt
-
-# Keras specific
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.utils import to_categorical
