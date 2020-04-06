8a044e600843e5b762e6a071ab76dc710cac300c
Maximilian Kroker <kroker@not-a.lawyer>
2020-04-06 17:19:59 +0200

    new article

--- rnn.py
+++ rnn.py
@@ -1,5 +1,6 @@
 ## Using Keras to classify some tweets as hate speech
-## Following this guide at https://www.pluralsight.com/guides/classification-keras
+## Following this guide at
+# https://medium.com/@armandj.olivares/detecting-toxic-comments-with-keras-and-interpreting-the-model-with-eli5-dbe734f3e86b
 
 # Import required libraries
 import pandas as pd
