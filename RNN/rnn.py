d0aed63f4ef63bebb0a1a20e6b3876ea5c5a4398
Maximilian Kroker <kroker@not-a.lawyer>
2020-04-09 16:38:19 +0200

    finished set_up_triple_lstm_model

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -7,6 +7,9 @@ from bayes_classifier import *
 from nltk.corpus import stopwords
 from keras.preprocessing.text import Tokenizer
 from keras.layers import Dense, Input, LSTM, Bidirectional, Activation, Conv1D, GRU, TimeDistributed, Dropout, Embedding, GlobalMaxPooling1D, MaxPooling1D, Add, Flatten, SpatialDropout1D
+from keras.optimizers import Adam
+from keras.models import Model
+from keras.losses import sparse_categorical_crossentropy
 
 #import insults.csv. see import_data() for details on data or README
 type_tweet = import_data()
@@ -54,15 +57,28 @@ def tokenize_tweets(type_tweet):
 
 
 
-def set_up_model():
+def set_up_triple_lstm_model():
     """
     Set up the keras model
     :return model:
     """
 
+    learning_rate = 1e-3
 
     input_tweet = Input(shape=(None,))
-    tweet_embedding = Embedding(10000, 200)
-    tweets = type_tweet["tweet"]
-    hate_speech_labels = type_tweet["tweet"]
-    pass
+    tweet_embedding = Embedding(5000, 200)(input_tweet)
+    lstm = LSTM(64, recurrent_dropout=0.2, return_sequences = True)(tweet_embedding)
+    dropout = Dropout(0.2)(lstm)
+    lstm2 = LSTM(64, recurrent_dropout=0.2,  return_sequences= True)(dropout)
+    dropout2 = Dropout(0.2)(lstm2)
+    output = Dense(2, activation='softmax')(dropout2)
+
+    model = Model(input_tweet, output)
+
+    model.compile(loss=sparse_categorical_crossentropy,
+                  optimizer=Adam(learning_rate),
+                  metrics=['accuracy'])
+    return model
+
+
+
