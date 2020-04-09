65ea9f59326b927b0f26ed9b0fec3f9d5d40d673
Maximilian Kroker <kroker@not-a.lawyer>
2020-04-09 16:43:22 +0200

    set up rnn classifier and testing on hate speech tweet

--- RNN/test_rnn.py
+++ RNN/test_rnn.py
@@ -16,6 +16,9 @@ class MyTestCase(unittest.TestCase):
         print(tokens.word_index)
         print(len(tokens.word_index))
         model = set_up_triple_lstm_model()
+        model.fit(tokenized_tweets["tweet"], tokenized_tweets["class"], batch_size=32, epochs=2, validation_split=0.2)
+
+        print(model.predict("She may or may not be a Jew but she 's certainly stupid , she seems to think the Blacks wo n't kill her alongside every other White they can get their dirty hands on , what a muppet !"))
         pass
 
 
