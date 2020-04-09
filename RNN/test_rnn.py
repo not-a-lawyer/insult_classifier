040ecf572cd2e1900ce5593a4b6c79e4e5e6ab03
Maximilian Kroker <kroker@not-a.lawyer>
2020-04-09 16:57:46 +0200

    added TODOs

--- RNN/test_rnn.py
+++ RNN/test_rnn.py
@@ -13,11 +13,14 @@ class MyTestCase(unittest.TestCase):
         preprocess_tweets_for_keras(type_tweet)
         tokenized_tweets, tokens = tokenize_tweets(type_tweet)
 
+        #TODO pad input
+
         print(tokens.word_index)
         print(len(tokens.word_index))
         model = set_up_triple_lstm_model()
         model.fit(tokenized_tweets, type_tweet["class"], batch_size=32, epochs=2, validation_split=0.2)
 
+        ##TODO tokenize and pad test hate speech
         print(model.predict("She may or may not be a Jew but she 's certainly stupid , she seems to think the Blacks wo n't kill her alongside every other White they can get their dirty hands on , what a muppet !"))
         pass
 
