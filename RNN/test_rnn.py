cf492f06e94b67c90bb9e0eaeec25878031c9ed2
Maximilian Kroker <kroker@not-a.lawyer>
2020-04-10 17:49:15 +0200

    copied general test into German test

--- RNN/test_rnn.py
+++ RNN/test_rnn.py
@@ -34,9 +34,28 @@ class MyTestCase(unittest.TestCase):
         pass
 
     def test_german(self):
+        #german data txt also needs to be copied into this subdirectory
         type_tweet = relabel_german_data()
 
-        pass
+        preprocess_tweets_for_keras(type_tweet)
+        tokenized_tweets, token = tokenize_tweets(type_tweet)
+
+        padded_tweets, padding_length = pad_tweets(tokenized_tweets)
+
+        input_length = len(padded_tweets)
+        print(token.word_index)
+        print(len(token.word_index))
+        model = set_up_triple_lstm_model(padding_length)
+        model.fit(padded_tweets, type_tweet["class"], batch_size=32, epochs=2, validation_split=0.2, verbose=1)
+
+        tokenized_test_tweet = token.texts_to_sequences(["""She may or may not be a Jew
+                 but she 's certainly stupid , she seems to think the Blacks wo n't kill her alongside every other 
+                 White they can get their dirty hands on , what a muppet !""", "Test", "Bitch",
+                                                         """DevilGrimz: @VigxRArts you're fucking gay, blacklisted hoe"" Holding out for #TehGodClan anyway http://t.co/xUCcwoetmn"""])
+
+        padded_test_tweet, _ = pad_tweets(tokenized_test_tweet, padding_length)
+
+        result = model.predict(padded_test_tweet)
 
 
 
