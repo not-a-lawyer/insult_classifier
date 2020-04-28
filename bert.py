9221da3a61c93d18d2663bbd5c8e45ca57464b4b
Maximilian Kroker <kroker@not-a.lawyer>
2020-04-28 13:44:44 +0200

    wrapping bert in main

---
+++ bert.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+import torch
+import transformers as ppb
+import warnings
+warnings.filterwarnings('ignore')
+
+def main():
+
+# For DistilBERT:
+model_class, tokenizer_class, pretrained_weights = (ppb.DistilBertModel, ppb.DistilBertTokenizer, 'distilbert-base-uncased')
